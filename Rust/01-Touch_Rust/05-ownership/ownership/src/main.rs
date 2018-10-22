86c626adcd51968ca69b011fbd6cb374743c9073
blubMe <fahmiirsyad10@gmail.com>
2018-10-22 12:37:53 +0700

    Update rust => ownering guide

---
+++ Rust/01-Touch_Rust/05-ownership/ownership/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let x = String::from("Ar you mai owner ?");
+    // ini merupakan contoh yang buruk , karena string "Ar you mai owner" berpindah owner
+    // x =[ownernya] => [value] , sedangkan jika kita pakai x = a
+    // x => [tidak bisa , kamu buka lagi ownernya] (XXX) || [value] <=[owner baru]= a
+    let a = x;
+
+    // lebih baik kita gunakan .clone() , berguna untuk menduplikat data / object dari variable yang diinginkan
+    // dengan ini kita bisa menghindari pemindahan ownership
+    // tetapi kenapa kok ga kita clone dari x ? akan terjadi error karena x bukan lagi pemilik , pemilik barunya adalah a [HOHOOH SUKURIN :D]
+    let a_clone = a.clone();
+
+    println!("{}",a_clone);
+}
