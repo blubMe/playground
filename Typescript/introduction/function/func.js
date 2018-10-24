003756cbecbaea995b5e41ad7bd910b2fcb1ee0c
blubMe <fahmiirsyad10@gmail.com>
2018-10-24 08:11:08 +0700

    Update typescript => functions

---
+++ Typescript/introduction/function/func.js
@@ -0,0 +1,8 @@
+function gretting(person) {
+    return "Hello, " + person;
+}
+var user = "My name is fahmi";
+// implementation ES6
+var arr = function (arry) { return "List array: " + arry; };
+var listarr = ["aku", "dia", "kita", "mereka"];
+console.log(gretting(user) + "\n" + arr(listarr));
---
+++ Typescript/introduction/function/func.ts
@@ -0,0 +1,13 @@
+function gretting(person: string){
+    return `Hello, ${person}`
+}
+
+let user = "My name is fahmi";
+
+// implementation ES6
+const arr = (arry: Array<string>) => `List array: ${arry}`
+let listarr = ["aku","dia","kita","mereka"]
+
+
+console.log(`${gretting(user)}
+${arr(listarr)}`);
