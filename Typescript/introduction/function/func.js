022c63b43b17050aa25c543d477292afd700d981
blubMe <fahmiirsyad10@gmail.com>
2018-10-24 09:31:23 +0700

    Update typescript => functions
    - add optional type
    - add behavior in optional param
    - es6 and ternary implementations

--- Typescript/introduction/function/func.js
+++ Typescript/introduction/function/func.js
@@ -8,11 +8,12 @@ var user = "My name is fahmi";
 var arr = function (arry) { return "List array: " + arry; };
 var listarr = ["aku", "dia", "kita", "mereka"];
 console.log(gretting(user) + "\n" + arr(listarr));
-function hasName(obj) {
-    return !!obj &&
-        typeof obj === "object" && "name" in obj;
-}
-var mix = 8;
-mix = true;
-mix = "Its mixed";
-hasName(mix) ? console.log(mix.name) : null;
+// optional params
+var arrOpt = function (obj1, obj2) {
+    var res = obj2 ? obj1 + " & " + obj2 : obj1;
+    return res;
+};
+var a = "Aku harus masuk";
+var b = "Aku optional";
+console.log(arrOpt(a, b));
+console.log(arrOpt(a));
--- Typescript/introduction/function/func.ts
+++ Typescript/introduction/function/func.ts
@@ -1,15 +1,26 @@
 export {}
 
-function gretting(person: string){
+function gretting(person: string): string {
     return `Hello, ${person}`
 }
 
 let user = "My name is fahmi";
 
 // implementation ES6
-const arr = (arry: Array<string>) => `List array: ${arry}`
+const arr = (arry: Array<string>): string => `List array: ${arry}`
 let listarr = ["aku","dia","kita","mereka"]
 
 console.log(`${gretting(user)}
 ${arr(listarr)}`);
 
+
+// optional params
+const arrOpt = (obj1: string, obj2?: string) : string => {
+    const res = obj2 ? `${obj1} & ${obj2}` : obj1
+    return res;
+}
+const a = "Aku harus masuk"
+const b = "Aku optional"
+
+console.log(arrOpt(a,b))
+console.log(arrOpt(a))
