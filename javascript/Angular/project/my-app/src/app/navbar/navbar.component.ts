8a16ae619babdb77dbfd0693e876068466108b29
blubMe <fahmiirsyad10@gmail.com>
2018-10-25 06:59:12 +0700

    Playing with angular

---
+++ javascript/Angular/project/my-app/src/app/navbar/navbar.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: '[app-navbar]',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+  navTitle: string = "Capsule"
+  myId: string = "capsuleID"
+  isDisabled: boolean = true
+  hasError: boolean = true
+  messageText: object = {
+    "text-success": !this.hasError,
+    "text-error": this.hasError
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+  greeting():string{
+    return "Im functions"
+  }
+  changeStat(event){
+    this.hasError = !this.hasError
+    console.log(this.hasError)
+    console.log(event)
+  }
+}
