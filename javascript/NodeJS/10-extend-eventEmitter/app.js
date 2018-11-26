21dddc4fc933a5b35227cbadb66d56131f7bb57a
blubMe <fahmiirsyad10@gmail.com>
2018-11-26 18:19:41 +0700

    Update nodejs => extend eventEmitter using class

--- javascript/NodeJS/10-extend-eventEmitter/app.js
+++ javascript/NodeJS/10-extend-eventEmitter/app.js
@@ -1,12 +1,16 @@
 const EventEmitter = require('events')
-const emitter = new EventEmitter()
+
+const Logger = require('./modules')
+const log = new Logger()
+
 const data = [
     {id: 1,url: 'http://facebook.com'},
     {id: 2,url: 'http://uber.com'},
     {id: 3,url: 'http://google.com'}
 ]
+
 // Regis
-emitter.on('messageCall',(arg) => console.log(arg))
+log.on('messageCall',(arg) => console.log(arg))
+
 // Create event
-emitter.emit('messageCall',data)
-// Raise
+log.fire(data)
--- javascript/NodeJS/10-extend-eventEmitter/modules.js
+++ javascript/NodeJS/10-extend-eventEmitter/modules.js
@@ -1,1 +1,13 @@
+const EventEmitter = require('events')
+
 var url = 'http://google.com'
+
+class Logger extends EventEmitter {
+    fire(msg) {
+        console.log(msg)
+        // Event
+        this.emit('messageCall',{id: 1 , url})
+    }
+}
+
+module.exports = Logger
